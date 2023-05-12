a962e445ffe623e351a85d1684b58f45afd45be5
Sergii <iakovyshyn.s@gmail.com>
2023-05-12 20:47:37 +0300

    new

--- components/MainPage.vue
+++ components/MainPage.vue
@@ -1,11 +1,18 @@
-<script setup>
-
-</script>
-
 <template>
-  $END$
+  <div>
+    <header>
+      <img class="logo" src="../assets/images/logo.svg" alt="logo"/>
+      <img class="logo_mobile" src="../assets/images/logo_mobile.svg" alt="logo ">
+    </header>
+    <main class="container">
+      <h1>«Дизайн під час війни»</h1>
+      <p class="description">Design Village повертається, щоб зустрітись 10-го червня у Івано-Франківську, і поговорити
+        про українські
+        візуальні комунікації під час повномасштабного вторгнення.</p>
+      <p class="description price">Early bird по 800 грн ↓</p>
+      <a href="https://2event.com/events/1866785#/">Купити квиток</a>
+    </main>
+    <footer>
+    </footer>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
---
+++ nuxt.config.js
@@ -0,0 +1,41 @@
+export default {
+  head: {
+    title: 'Design Village 2023 — “Дизайн під час війни”',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Подія, на якій 10.06 відомі українські дизайнери поділяться своїм досвідом створення візуальних комунікацій під час повномасштабного вторгнення. Івано-Франківськ.' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  target: 'static',
+  css: [
+    '~/assets/scss/normalize.css',
+    '~/assets/scss/main.scss'
+  ],
+
+  plugins: [
+  ],
+
+  components: true,
+
+  buildModules: [
+  ],
+
+  modules: [
+    '@nuxtjs/axios',
+  ],
+
+  axios: {
+    baseURL: '/',
+  },
+
+  build: {
+  }
+}
